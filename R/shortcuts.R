ea584e83fac07f7fe3d473f73ff0422b585ed18e
Andy Teucher <andy.teucher@gmail.com>
2017-05-05 12:25:01 -0700

    Add bc_albers
    And correct a typo

--- R/shortcuts.R
+++ R/shortcuts.R
@@ -16,11 +16,20 @@ geo_wgs84 <- function(){
   "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 }
 
-#' USGS Albers Equal Area Proejction proj.4 string
+#' USGS Albers Equal Area Projection proj.4 string
 #' 
 #' Function that provides a proj.4 string
 #' 
 #' @export
 usgs_albers <- function(){
   "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+}
+
+#' BC Albers Equal Area Projection proj.4 string for British Columbia
+#' 
+#' Function that provides a proj.4 string
+#' 
+#' @export
+bc_albers <- function() {
+  "+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
 }
