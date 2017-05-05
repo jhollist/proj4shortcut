07cf65388828cf88d23bc6ef6a54582f327f9621
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-05-05 14:35:48 -0400

    first cut

---
+++ R/shortcuts.R
@@ -0,0 +1,26 @@
+#' Web Mercator proj.4 string
+#' 
+#' Function that provides a proj.4 string
+#' 
+#' @export
+web_mercator <- function(){
+  "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"
+}
+
+#' Geographic, decimal degrees, wgs84 proj.4 string
+#' 
+#' Function that provides a proj.4 string
+#' 
+#' @export
+geo_wgs84 <- function(){
+  "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+}
+
+#' USGS Albers Equal Area Proejction proj.4 string
+#' 
+#' Function that provides a proj.4 string
+#' 
+#' @export
+usgs_albers <- function(){
+  "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+}
